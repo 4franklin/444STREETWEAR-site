b69ebbbaabe69fb37e87d475b29ab4ba4a03674b
4franklin <aiselfranklin444@gmail.com>
2026-03-23 20:23:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("active");
+}
+
+let cart = [];
+
+function addToCart(nome, preco) {
+    cart.push({ nome, preco });
+    updateCart();
+}
+
+function updateCart() {
+    const lista = document.getElementById("cart-items");
+    const total = document.getElementById("total");
+
+    lista.innerHTML = "";
+    let soma = 0;
+
+    cart.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = `${item.nome} - ${item.preco} MT`;
+        lista.appendChild(li);
+        soma += item.preco;
+    });
+
+    total.textContent = soma;
+}
+
+function checkout() {
+    let mensagem = "🛒 *Pedido 444STREETWEAR*%0A%0A";
+
+    cart.forEach(item => {
+        mensagem += `- ${item.nome} (${item.preco} MT)%0A`;
+    });
+
+    const total = document.getElementById("total").textContent;
+
+    mensagem += `%0A💰 Total: ${total} MT`;
+
+    window.open(`https://wa.me/258867335175?text=${mensagem}`);
+}
+
+function openModal(img1, img2 = "") {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("img1").src = img1;
+    document.getElementById("img2").src = img2;
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
